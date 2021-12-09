ab83a2605b93fd12ab7b4f48a26e50bd546592c2
Albaer33 <alisi3396@gmail.com>
2021-12-09 17:12:14 +0100

    MILESTONE 1 completata

--- js/script.js
+++ js/script.js
@@ -4,9 +4,23 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-
+            tasks: [
+                {
+                    done: false,
+                    text: 'fare spesa'
+                },
+                {
+                    done: true,
+                    text: 'preparare colazione'
+                },
+                {
+                    done: false,
+                    text: 'fare esercizi'
+                },
+            ] 
         },
         methods: {
+
         }
     }
 );
